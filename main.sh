349d8e1a0240bac86220cda88f272ef2ef164287
Peter Shao <pshao@studentbridge.com>
2024-02-28 19:55:22 -0500

    schema selection works!

--- main.sh
+++ main.sh
@@ -3,12 +3,13 @@
 logins_file='logins.gpg'
 logins=''
 env='local'
+selected_schema=''
 host=''
 user=''
 pass=''
 
+
 schemas=()
-selected_schema=()
 tables=()
 selected_tables=()
 filename=()
@@ -24,6 +25,7 @@ decrypt_credentials() {
 
 assign_credentials() {
     env="$1"
+    selected_schema=''
     read host user pass <<<$(echo "$logins" | awk -F',' -v Environment="$env" '$1 == Environment {print $2" "$3" "$4}')
 }
 
@@ -65,56 +67,23 @@ change_environment_menu() {
 }
 
 select_schema_menu() {
-    echo "Loading data..."
+    #echo "Loading data..."
     schemas=$(timeout 10 mysql -h "$host" -u "$user" -p"$pass" -e "SHOW DATABASES;" | awk '{if (NR>1) print $1}')
-    echo ""
+    #echo ""
 
     if [ -n "$schemas" ]; then
-        echo "Schemas on $env: "
-        echo ""
-        #echo "$schemas"
-
-        declare -a options
-        # Read databases into options array
-        IFS=$'\n' read -d '' -r -a options <<< "$(printf '%s\n' "$schemas")"
-        selection=$(dialog --stdout --title "Select Schema" --menu "Choose a schema:" 20 60 10 "${options[@]}")
-
-
+        #echo "Schemas on $env: "
+        #echo ""
+        options=()
+        while IFS= read -r schema_name; do
+            options+=("$schema_name" "")
+        done <<< "$schemas"
+        selected_schema=$(dialog --stdout --title "Select Schema" --menu "Choose a schema:" 20 60 10 "${options[@]}")
     else
-        # The command timed out or encountered an error
+        #TODO: Make this a dialog screen with an ok button
         echo "Error: Timeout occurred or command failed. Please check connection and/or credentials."
         echo ""
     fi
-}
-
-old_select_schema_menu() {
-    echo "Loading data..."
-    schemas=$(timeout 10 mysql -h "$host" -u "$user" -p"$pass" -e "SHOW DATABASES;" | awk '{if (NR>1) print $1}')
-    echo ""
-
-    echo "Schemas on $env: "
-    echo ""
-
-    options=()
-    if [ -n "$schemas" ]; then
-        echo "$schemas" | while read -r schema_name; do
-            options+=("$schema_name" "$schema_name")
-        done
-    else
-        echo "No schemas found."
-    fi
-
-    selection=$(dialog --stdout --title "Select Schema" --menu "Choose a schema:" 20 60 10 "${options[@]}")
-
-    if [ -n "$selection" ]; then
-        echo "Selected schema: $selection"
-        # Set the globally defined $schema variable to the selected schema
-        schema="$selection"
-        echo "Globally defined schema set to: $schema"
-    else
-        echo "No schema selected."
-    fi
-    echo ""
 }
 
 select_tables_menu() {
@@ -134,8 +103,8 @@ load_dump_file_menu() {
 decrypt_credentials
 assign_credentials $env
 
-#while true; do
-    prompt="Environment: $env\nDatabase: $selected_schema\nTables: $selected_tables\nFilename: $filename"
+while true; do
+    prompt="Environment: $env\nSchema: $selected_schema\nTables: $selected_tables\nFilename: $filename"
     options=(
         1 "Change Environment"
         2 "Select Schema"
@@ -178,4 +147,4 @@ assign_credentials $env
         echo "Invalid option. Please select again."
         ;;
     esac
-#done
+done
